35c8523f0420de61e6e93c20b35bbff8d97a6e56
nhduy9x <nhduy9x@gmail.com>
2019-07-25 22:07:03 +0700

    assignment

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,6 +53,6 @@ class LoginController extends Controller
     }
      public function logout(){
         Auth::logout();
-        return redirect(route('homepage'));
+        return redirect(route('home'));
     }
 }
